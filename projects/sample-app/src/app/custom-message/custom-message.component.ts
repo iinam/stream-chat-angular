37db38670fec02ca80bbe55dbeb9ba6712726873
Zita Szupera <szuperaz@gmail.com>
2022-03-30 14:51:23 +0200

    Example for custom message template

--- projects/sample-app/src/app/custom-message/custom-message.component.ts
+++ projects/sample-app/src/app/custom-message/custom-message.component.ts
@@ -1,21 +1,9 @@
-import { Component, Input } from '@angular/core';
-import { FormatMessageResponse } from 'stream-chat';
-import { ChatClientService } from 'stream-chat-angular';
+import { Component } from '@angular/core';
+import { MessageComponent } from 'stream-chat-angular';
 
 @Component({
   selector: 'app-custom-message',
   templateUrl: './custom-message.component.html',
   styleUrls: ['./custom-message.component.scss'],
 })
-export class CustomMessageComponent {
-  @Input() message: FormatMessageResponse | undefined;
-  userId: string | undefined;
-
-  constructor(private chatClientService: ChatClientService) {
-    this.userId = this.chatClientService.chatClient.user?.id;
-  }
-
-  get isSentByCurrentUser() {
-    return this.message?.user?.id === this.userId;
-  }
-}
+export class CustomMessageComponent extends MessageComponent {}
